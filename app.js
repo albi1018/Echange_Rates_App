2a2b68192fb35b0abbdd21d57dca198bbf9a779c
albi1018 <albi1018@gmail.com>
2021-08-23 21:06:13 +0200

    add search

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ window.onload = function () {
     rates.init();
 }
 
+
+
 class ExchangeRates {
     url = "https://api.nbp.pl/api/exchangerates/tables/a/last/?format=json";
-
     tBody = document.querySelector("#ratesTable tbody");
 
     init() {
@@ -21,6 +22,7 @@ class ExchangeRates {
 
     parseData(data) {
         data = data[0];
+        console.log(data);
         this.table = data.table;
         this.date = data.effectiveDate;
         this.no = data.no;
@@ -37,13 +39,35 @@ class ExchangeRates {
     addRateToTable(el) {
         let tr = document.createElement("tr");
         tr.innerHTML = `
-            <td class="align-middle">${el.code}</td>
-            <td class="align-middle">${el.currency}</td>
-            <td class="align-middle">${el.mid} zł</td>
+            <td class="align-middle code">${el.code}</td>
+            <td class="align-middle currency">${el.currency}</td>
+            <td class="align-middle mid">${el.mid} zł</td>
         `
 
         this.tBody.appendChild(tr);
     }
+
+    search () {
+        const inputSearch = document.querySelector("input");
+        console.log(inputSearch);
+
+
+        inputSearch.addEventListener("input", () => {
+            const val = inputSearch.value.toLowerCase();
+            const elem = document.querySelectorAll("tbody tr");
+            for (const el of elem) {
+                const text = el.querySelector(".currency").innerText;
+                const code = el.querySelector(".code").innerText;
+                const number = el.querySelector(".mid").innerText;
+                if (text.includes(val) || code.toLowerCase().includes(val) || number.includes(val)) {
+                    el.style.setProperty("display", "");
+                } else {
+                    el.style.setProperty("display", "none");
+                }
+            }
+        })
+    }
 }
 
 const rates = new ExchangeRates();
+rates.search();
